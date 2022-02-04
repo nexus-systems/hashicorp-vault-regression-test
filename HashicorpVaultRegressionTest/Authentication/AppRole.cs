648f21dbfc943c554cd4e4e6f4cb31b13dd668c7
Ross Nesbit <rnesbit@nexus-network.cloud>
2022-02-04 10:15:18 -0600

    First Commit

---
+++ HashicorpVaultRegressionTest/Authentication/AppRole.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashicorpVaultRegressionTest.Authentication
+{
+    [TestClass]
+    [TestCategory("Authentication")]
+    public class AppRole
+    {
+        protected static TestContext _context;
+
+        [ClassInitialize]
+        public static void InitClass(TestContext context)
+        {
+            _context = context;
+        }
+
+        [TestMethod("AppRole Basic Lifecycle")]
+        public async Task AppRoleBasicLifecycleAsync()
+        {
+
+        }
+
+        private async Task ListApproleAsync()
+        {
+
+        }
+
+        private async Task CreateUpdateAppRoleAsync()
+        {
+
+        }
+
+        private async Task ReadAppRoleAsync()
+        {
+
+        }
+
+        private async Task DeleteAppRoleAsync()
+        {
+
+        }
+    }
+}
